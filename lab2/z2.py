355f90738780640c8ac0461acb7b682b031317d6
Dawid Roszman <dawidroszman@gmail.com>
2024-04-10 18:47:09 +0200

    working on z2 and z3

--- lab2/z2.py
+++ lab2/z2.py
@@ -1,8 +1,13 @@
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
 
 
 df = pd.read_csv("./iris1.csv")
+
 features = ["sepal.length", "sepal.width", "petal.length", "petal.width"]
 
 # Separating out the features
@@ -13,4 +18,72 @@ y = df.loc[:, ["variety"]].values
 
 # Standardizing the features
 x = StandardScaler().fit_transform(x)
-print(x)
+
+
+pca = PCA(n_components=2)
+
+principalComponents = pca.fit_transform(x)
+
+retained_variance = np.sum(pca.explained_variance_ratio_)
+percentage_retained = retained_variance * 100
+print(percentage_retained)
+
+principalDf = pd.DataFrame(
+    data=principalComponents, columns=["principal component 1", "principal component 2"]
+)
+
+finalDf = pd.concat([principalDf, df[["variety"]]], axis=1)
+fig = plt.figure(figsize=(8, 8))
+ax = fig.add_subplot(1, 1, 1)
+ax.set_xlabel("Principal Component 1", fontsize=15)
+ax.set_ylabel("Principal Component 2", fontsize=15)
+ax.set_title("2 component PCA", fontsize=20)
+
+targets = ["Setosa", "Versicolor", "Virginica"]
+colors = ["r", "g", "b"]
+for target, color in zip(targets, colors):
+    indicesToKeep = finalDf["variety"] == target
+    ax.scatter(
+        finalDf.loc[indicesToKeep, "principal component 1"],
+        finalDf.loc[indicesToKeep, "principal component 2"],
+        c=color,
+        s=50,
+    )
+ax.legend(targets)
+ax.grid()
+
+plt.show()
+
+
+pca = PCA(n_components=3)  # Setting n_components to 3 for three principal components
+principalComponents = pca.fit_transform(x)
+
+retained_variance = np.sum(pca.explained_variance_ratio_)
+percentage_retained = retained_variance * 100
+print(percentage_retained)
+
+principalDf = pd.DataFrame(data=principalComponents, columns=["PC1", "PC2", "PC3"])
+
+finalDf = pd.concat([principalDf, df[["variety"]]], axis=1)
+
+# Creating the 3D plot
+fig = plt.figure(figsize=(10, 8))
+ax = fig.add_subplot(111, projection="3d")
+ax.set_title("3D PCA")
+
+for target, color in zip(targets, colors):
+    indicesToKeep = finalDf["variety"] == target
+    ax.scatter(
+        finalDf.loc[indicesToKeep, "PC1"],
+        finalDf.loc[indicesToKeep, "PC2"],
+        finalDf.loc[indicesToKeep, "PC3"],
+        c=color,
+        s=50,
+        label=target,
+    )
+
+ax.set_xlabel("PC1")
+ax.set_ylabel("PC2")
+ax.set_zlabel("PC3")
+ax.legend()
+plt.show()
---
+++ lab2/z3.py
@@ -0,0 +1,92 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+
+df = pd.read_csv("./iris1.csv")
+
+print(df)
+
+fig = plt.figure(figsize=(8, 8))
+ax = fig.add_subplot(1, 1, 1)
+
+ax.set_xlabel("Sepal Length")
+ax.set_ylabel("Sepal Width")
+ax.set_title("Sepal")
+
+targets = ["Setosa", "Versicolor", "Virginica"]
+colors = ["r", "g", "b"]
+
+for target, color in zip(targets, colors):
+    indicesToKeep = df["variety"] == target
+    ax.scatter(
+        df.loc[indicesToKeep, "sepal.length"],
+        df.loc[indicesToKeep, "sepal.width"],
+        c=color,
+        s=50,
+    )
+ax.legend(targets)
+ax.grid()
+
+plt.show()
+
+features = ["sepal.length", "sepal.width"]
+x = df[features].values
+
+y = df["variety"].values
+
+# Scales each feature to have mean of 0 and sd of 1
+scaler = StandardScaler()
+x_normalized = scaler.fit_transform(x)
+
+fig = plt.figure(figsize=(8, 8))
+ax = fig.add_subplot(1, 1, 1)
+
+ax.set_xlabel("Sepal Length (Normalized)")
+ax.set_ylabel("Sepal Width (Normalized)")
+ax.set_title("Normalized Sepal")
+
+targets = ["Setosa", "Versicolor", "Virginica"]
+colors = ["r", "g", "b"]
+
+for target, color in zip(targets, colors):
+    indicesToKeep = df["variety"] == target
+    ax.scatter(
+        x_normalized[indicesToKeep, 0],
+        x_normalized[indicesToKeep, 1],
+        c=color,
+        s=50,
+    )
+ax.legend(targets)
+ax.grid()
+
+plt.show()
+
+# Scaler transforms features by scaling each feature to a given range,
+# It computes minimum and maximum values of each feature and
+# then scales the values to the specified range
+# Default is 0 an 1
+scaler = MinMaxScaler()
+x_normalized = scaler.fit_transform(x)
+
+fig = plt.figure(figsize=(8, 8))
+ax = fig.add_subplot(1, 1, 1)
+
+ax.set_xlabel("Sepal Length (Normalized)")
+ax.set_ylabel("Sepal Width (Normalized)")
+ax.set_title("Normalized Sepal")
+
+targets = ["Setosa", "Versicolor", "Virginica"]
+colors = ["r", "g", "b"]
+
+for target, color in zip(targets, colors):
+    indicesToKeep = df["variety"] == target
+    ax.scatter(
+        x_normalized[indicesToKeep, 0],
+        x_normalized[indicesToKeep, 1],
+        c=color,
+        s=50,
+    )
+ax.legend(targets)
+ax.grid()
+
+plt.show()
